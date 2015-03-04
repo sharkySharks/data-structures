2f28df0bda997fbeabe3a355a450ac048a27e6b7
MKS-Student <mks.student@hackreactor.com>
2015-03-04 14:24:13 -0800

    Finished functional construction for stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,37 @@
 var Queue = function(){
   var someInstance = {};
 
+
   // Use an object with numeric keys to store values
+  var size = 0;
+  var first = 0;
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+     storage[size] = value;
+     size++;
   };
 
   someInstance.dequeue = function(){
+    var result = storage[first];
+    delete storage[first];
+    for (var k in storage){
+      console.log("I'm a key in the queue:" + k);
+    }
+    first++;
+    return result;
   };
 
   someInstance.size = function(){
-  };
 
+    if ((size - first) >=0){
+      return size - first;
+    }
+    else{
+      return 0;
+    }
+  };
   return someInstance;
 };
